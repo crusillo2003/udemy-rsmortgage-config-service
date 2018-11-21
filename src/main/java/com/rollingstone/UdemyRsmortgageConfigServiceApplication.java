0d64773154d7f965ce5e385a646a6ce708582113
ubaldo <ubaldocj.84@gmail.com>
2018-11-20 18:43:30 -0600

    first commit

---
+++ src/main/java/com/rollingstone/UdemyRsmortgageConfigServiceApplication.java
@@ -0,0 +1,16 @@
+package com.rollingstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableConfigServer
+public class UdemyRsmortgageConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyRsmortgageConfigServiceApplication.class, args);
+	}
+}
